4a3ccfcbeb35128d0e601489d040b7bc0a06dffb
imadghssisse <ghssissea@gmail.com>
2023-06-20 13:25:15 +0100

    fix package

--- src/package/node_modules/react-drag-reorder/types/components/draggable/draggable.d.ts
+++
@@ -1,6 +0,0 @@
-import { ReactElement, ReactNode } from "react";
-
-export default function DraggableComponent(props: {
-    children?: ReactNode;
-    onPosChange?: (currentPos: number, newPos: number) => void;
-}): ReactElement;
